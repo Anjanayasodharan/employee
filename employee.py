f80ac966c6e564cfd115e7c7b774c3423a66af63
anjanayasodharan89@gmail.com <anjanayasodharan89@gmail.com>
2022-11-02 10:38:56 +0530

    view

--- employee.py
+++ employee.py
@@ -25,7 +25,11 @@ while True:
         mycursor.execute(sql , data)
         mydb.commit()
     elif(choice==2):
-        print('view employee')
+        sql = 'SELECT * FROM `employees`'
+        mycursor.execute(sql)
+        result = mycursor.fetchall()
+        for i in result:
+            print(i)
     elif(choice==3):
         print('search employee')
     elif(choice==4):
